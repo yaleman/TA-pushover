cd285eadf3be5853b5e206c0d29149d211198bb3
James Hodgkinson <james@terminaloutcomes.com>
2022-04-09 21:03:19 +1000

    yolo the splunklibben

--- TA-pushover/bin/pushover.py
+++ TA-pushover/bin/pushover.py
@@ -2,7 +2,13 @@
 
 # import ta_pushover_declare
 
+import json
 import sys
+
+from urllib.parse import urlparse
+# import requests
+
+from .splunklib import client
 # import traceback
 
 # from ta_pushover.alert_actions_base import ModularAlertBase
@@ -44,7 +50,44 @@ import sys
 if __name__ == "__main__":
     for value in sys.argv:
         print(f"argv: {value}", file=sys.stderr)
-    for line in sys.stdin.readlines():
-        print(f"stdin: {line}", file=sys.stderr)
+    stdin = sys.stdin.read()
 
+    print(f"stdin: {json.dumps(stdin)}", file=sys.stderr)
+
+    config = json.loads(stdin)
+
+    headers = {
+        "Authorization" : f"Bearer {config['session_key']}",
+    }
+
+    parsed_url = urlparse(config["server_uri"])
+
+
+    client.connect(
+        host=parsed_url.hostname,
+        port=parsed_url.port,
+        scheme=parsed_url.scheme,
+        token=config["session_key"]
+    )
+    # client.connect(token=)
+# stdin: {
+# "app":"TA-pushover",
+# "owner":"admin",
+# "results_file":"/opt/splunk/var/run/splunk/dispatch/scheduler__admin_VEEtcHVzaG92ZXI__spam_at_1649498040_4/sendalert_temp_results.csv.gz",
+# "results_link":"http://22072aa942a9:8000/app/TA-pushover/@go?sid=scheduler__admin_VEEtcHVzaG92ZXI__spam_at_1649498040_4",
+# "search_uri":"/servicesNS/admin/TA-pushover/saved/searches/spam",
+# "server_host": "22072aa942a9",
+# "server_uri": "https://127.0.0.1:8089",
+# "session_key": "aYAUg6hQDWJyV_UuSlXWEsgwUhFE5UasbI1vzUSiaV8DZAu7aah49lrZog^BxMgcUmIj6fe8p6wPxcA7DfUo6LgZk5g^z^VV2ePsbtBNpKGG7zr4QrdNSIVgxTQ9aOE6rGlv^CpDLMzRDsF1BmU0zNmaQDuF3S9VmZSMnG_Dm_kySo",
+# "sid":"scheduler__admin_VEEtcHVzaG92ZXI__spam_at_1649498040_4",
+# "search_name":"spam",
+# "configuration":{
+#   "additional_url":"additional_url",
+#   "message":"Message - hello world",
+#   "priority":"0",
+#   "sound":"_",
+#   "title":"title",
+#   "url_title":"URL"
+# },
+# "result":{"_time":"1649498041","message":"hello world"}}
     # sys.exit(AlertActionWorkerpushover("TA-pushover", "pushover").run(sys.argv))
