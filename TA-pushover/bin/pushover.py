8aecd6ae486b9f6a111a5dd89b147d5876cc709f
James Hodgkinson <james@terminaloutcomes.com>
2022-04-09 19:03:16 +1000

    yolo this time

--- TA-pushover/bin/pushover.py
+++ TA-pushover/bin/pushover.py
@@ -3,43 +3,46 @@
 # import ta_pushover_declare
 
 import sys
-import traceback
+# import traceback
 
-from ta_pushover.alert_actions_base import ModularAlertBase
-from ta_pushover import modalert_pushover_helper
+# from ta_pushover.alert_actions_base import ModularAlertBase
+# from ta_pushover import modalert_pushover_helper
 
-class AlertActionWorkerpushover(ModularAlertBase):
-    """ the alert action """
+# class AlertActionWorkerpushover(ModularAlertBase):
+#     """ the alert action """
 
-    def validate_params(self):
-        """ validates parameters """
-        if not self.get_global_setting("user_key"):
-            self.log_error('user_key is a mandatory setup parameter, but its value is None.')
-            return False
+#     def validate_params(self):
+#         """ validates parameters """
+#         if not self.get_global_setting("user_key"):
+#             self.log_error('user_key is a mandatory setup parameter, but its value is None.')
+#             return False
 
-        if not self.get_param("message"):
-            self.log_error('message is a mandatory parameter, but its value is None.')
-            return False
-        return True
+#         if not self.get_param("message"):
+#             self.log_error('message is a mandatory parameter, but its value is None.')
+#             return False
+#         return True
 
-    def process_event(self, *args, **kwargs):
-        """ actually does the thing """
-        status = 0
-        try:
-            if not self.validate_params():
-                return 3
-            status = modalert_pushover_helper.process_event(self, *args, **kwargs)
-        except (AttributeError, TypeError) as attribute_error:
-            self.log_error(f"Error: {str(attribute_error)}. Please double check spelling and also verify that a compatible version of Splunk_SA_CIM is installed.")
-            return 4
-        except Exception as error: #pylint: disable=broad-except
-            msg = "Unexpected error: {}."
-            if error:
-                self.log_error(msg.format(str(error)))
-            else:
-                self.log_error(msg.format(traceback.format_exc()))
-            return 5
-        return status
+#     def process_event(self, *args, **kwargs):
+#         """ actually does the thing """
+#         status = 0
+#         try:
+#             if not self.validate_params():
+#                 return 3
+#             status = modalert_pushover_helper.process_event(self, *args, **kwargs)
+#         except (AttributeError, TypeError) as attribute_error:
+#             self.log_error(f"Error: {str(attribute_error)}. Please double check spelling and also verify that a compatible version of Splunk_SA_CIM is installed.")
+#             return 4
+#         except Exception as error: #pylint: disable=broad-except
+#             msg = "Unexpected error: {}."
+#             if error:
+#                 self.log_error(msg.format(str(error)))
+#             else:
+#                 self.log_error(msg.format(traceback.format_exc()))
+#             return 5
+#         return status
 
 if __name__ == "__main__":
-    sys.exit(AlertActionWorkerpushover("TA-pushover", "pushover").run(sys.argv))
+    for value in sys.argv:
+        print(value)
+
+    # sys.exit(AlertActionWorkerpushover("TA-pushover", "pushover").run(sys.argv))
