6372c586ee4324649c74283a507d8f1959bd9ab4
James Hodgkinson <james@terminaloutcomes.com>
2022-04-10 23:48:04 +1000

    making appinspect slightly happier

--- package.sh
+++ package.sh
@@ -8,6 +8,11 @@ poetry run find "${APP_DIR}/" -type f -name '*.conf' -exec ksconf check {} \;
 
 echo "Creating ${APP_DIR}.tgz"
 
-COPYFILE_DISABLE=1 tar czf "${APP_DIR}.tgz" "${APP_DIR}/"
+if [ -d "${APP_DIR}/bin/ta_pushover/aob_py3/lib2to3/tests/" ]; then
+    echo "Removing ${APP_DIR}/bin/ta_pushover/aob_py3/lib2to3/tests/"
+    rm -rf "${APP_DIR}/bin/ta_pushover/aob_py3/lib2to3/tests/"
+fi
+find . -type d -name __pycache__ -delete
+COPYFILE_DISABLE=1 tar czf "${APP_DIR}.tgz" --exclude '*/__pycache__/*' "${APP_DIR}/"
 
 echo "Done!"
