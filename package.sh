f411beb2ae84c643bcbd83fd0980f429d2ecd65e
James Hodgkinson <james@terminaloutcomes.com>
2022-04-10 22:20:05 +1000

    updated package and packaging

--- package.sh
+++ package.sh
@@ -2,7 +2,12 @@
 
 #python3 -m pip install -r requirements.txt
 
-find . -type f -name '*.conf' -exec ksconf check {} \;
+APP_DIR="TA-pushover"
 
-tar czf TA-pushover.tgz TA-pushover/
+poetry run find "${APP_DIR}/" -type f -name '*.conf' -exec ksconf check {} \;
 
+echo "Creating ${APP_DIR}.tgz"
+
+COPYFILE_DISABLE=1 tar czf "${APP_DIR}.tgz" "${APP_DIR}/"
+
+echo "Done!"
