a4f140651ee92a817eca1f44aa17ca803768bd17
James Hodgkinson <james@terminaloutcomes.com>
2022-04-13 22:15:56 +1000

    scripty buildy script thing

--- ucc-build.sh
+++ ucc-build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+	echo "Usage: ucc-build.sh [--build]"
+	echo ""
+	echo "Regenerates the UCC app files."
+	echo ""
+	echo "  Options:"
+	echo ""
+	echo "  --build    Build the .tgz package"
+	echo "  --help     Show this help"
+	echo ""
+	exit
+fi
+
 TA_VERSION="$(grep -E ^version pyproject.toml | awk '{print $NF}' | sed -E 's/"//g')"
 APP_DIR="TA-pushover/"
 
